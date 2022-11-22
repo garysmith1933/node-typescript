54551e4b8c785cc24f4251c7d0c6c1a95cbd5e96
Gary Smith <garysmith193358@gmail.com>
2022-11-22 09:14:26 -0500

    added CRUD functionality

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const todos_1 = require("../controllers/todos");
 //const express = require('express')
 //const Router = express.Router
 const router = (0, express_1.Router)();
-router.post('/');
-router.get('/');
-router.patch('/:id');
-router.delete('/:id');
+router.post('/', todos_1.createTodo);
+router.get('/', todos_1.getTodos);
+router.patch('/:id', todos_1.updateTodo);
+router.delete('/:id', todos_1.deleteTodo);
 exports.default = router;
